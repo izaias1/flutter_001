9fd6d38efbad376455ccc2f92075589aa26d5a50
izaias1 <izaiasfrancisco1@gmail.com>
2022-11-25 05:26:05 -0300

    Componentes Externos

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,33 @@ class MeuApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Meu Primeiro App'),
         ),
-        body: const Center(
-          child: Text('...'),
-        ),
+        body: Column(children: [
+          Exemplo(),
+          Exemplo(),
+        ]),
       ),
+    );
+  }
+}
+
+class Exemplo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      color: Colors.red,
+      child: Column(children: [
+        const Text('Texto de Exemplo'),
+        TextButton(
+          onPressed: () {},
+          child: const Text(
+            "Clique aqui",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ]),
     );
   }
 }
