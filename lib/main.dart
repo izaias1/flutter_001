f1bda26a8df34ab393827e0d039e262351d22851
izaias1 <izaiasfrancisco1@gmail.com>
2022-11-25 06:18:45 -0300

    Estilização

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ import 'package:flutter_001/exemplo.dart';
 void main() => runApp(MeuApp());
 
 class MeuApp extends StatelessWidget {
+  var textStyle = const TextStyle(
+    fontSize: 30,
+    color: Colors.black87,
+    fontWeight: FontWeight.bold,
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,15 +17,26 @@ class MeuApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Meu Primeiro App'),
         ),
-        body: Column(
-          children: [
-            const Image(
-              width: 100,
-              image: AssetImage('assets/logo.png'),
-            ),
-            Image.network(
-                'https://www.freepnglogos.com/uploads/logo-ifood-png/ifood-faca-seu-pedido-online-logo-png-12.png'),
-          ],
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                'Titulo da Pagina',
+                style: textStyle,
+              ),
+              Container(
+                child: const Icon(
+                  Icons.star,
+                  size: 40,
+                  color: Colors.blue,
+                ),
+              ),
+              TextButton(
+                onPressed: null,
+                child: Text('Clique aqui'),
+              ),
+            ],
+          ),
         ),
       ),
     );
